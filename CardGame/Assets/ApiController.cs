e75593783d74e8844e00b277991225edde47db3d
Bryant Jackson <bjackson_seller@outlook.com>
2016-05-25 19:49:20 -0400

    Created api controller Added GetCardInfo method Created basic card infrastructure Cards can get their information from api

---
+++ CardGame/Assets/ApiController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+using System;
+
+public class ApiController : MonoBehaviour {
+
+    public string baseUrl;
+    public string cardUrl;
+
+    private bool hasResult;
+    public string result;
+
+	// Use this for initialization
+	void Start () {
+        hasResult = false;
+	}
+	
+    public IEnumerator GetCardInfo(int id)
+    {
+        string call = cardUrl + id.ToString();
+        yield return GetApiJSONResults(call);
+    }
+
+	IEnumerator GetApiJSONResults(string call)
+    {
+        hasResult = false;
+        string url = baseUrl + call;
+        WWW web = new WWW(url);
+        yield return web;
+        if (web.error == null)
+        {
+            hasResult = true;
+            result = web.text;
+        }
+        else
+        {
+            Debug.Log("ApiController.GetApiJSONResults: Could not read from " + url + "!");
+            hasResult = true;
+            result = "";
+        }
+    }
+}
---
+++ CardGame/Assets/CardSetup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardSetup : MonoBehaviour {
+
+    public int cardId;
+
+    private ApiController api;
+
+	// Use this for initialization
+	IEnumerator Start () {
+        api = GameObject.FindObjectOfType<ApiController>();
+        yield return api.GetCardInfo(cardId);
+        Debug.Log("CardSetup.Start: " + api.result);
+	}
+
+}
