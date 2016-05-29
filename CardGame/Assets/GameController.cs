0622836dff892b617102c12dbf4196f6dd930f78
Bryant Jackson <bjackson_seller@outlook.com>
2016-05-29 09:53:11 -0400

    Adjusted game layout. Players are rotated around center point.

--- CardGame/Assets/GameController.cs
+++ CardGame/Assets/GameController.cs
@@ -10,6 +10,7 @@ public class GameController : MonoBehaviour {
 
     public string json;
     public int gameId;
+    public float offsetFromCenter;
 
 	// Use this for initialization
 	void Start () {
@@ -26,17 +27,31 @@ public class GameController : MonoBehaviour {
         while(i < gameInfo["players"].Count)
         {
             Debug.Log("generate a player");
-            yield return GeneratePlayer(int.Parse(gameInfo["players"][i]));
+            yield return GeneratePlayer(int.Parse(gameInfo["players"][i]), i, gameInfo["players"].Count);
             i++;
         }
     }
 
-    IEnumerator GeneratePlayer(int id)
+    IEnumerator GeneratePlayer(int id, int playerId, int numPlayers)
     {
+        float rotation = (360 / numPlayers) * playerId;
+        Debug.Log("rotating: " + rotation);
         GameObject entity = GameObject.Instantiate(statePrefab) as GameObject;
         StateSetup entityState = entity.GetComponent<StateSetup>();
         entityState.game = gameId;
         entityState.player = id;
         yield return entityState.GenerateHand();
+        //entity.transform.localPosition = entity.transform.localPosition + (Vector3.down * offsetFromCenter);
+        Vector3 newPosition = RotateVector2D(Vector3.down * offsetFromCenter, rotation);
+        entity.transform.localPosition = entity.transform.localPosition + newPosition;
+        entity.transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + (Vector3.forward * rotation));
+    }
+
+    private Vector3 RotateVector2D(Vector3 oldDirection, float angle)
+    {
+        float newX = Mathf.Cos(angle * Mathf.Deg2Rad) * (oldDirection.x) - Mathf.Sin(angle * Mathf.Deg2Rad) * (oldDirection.y);
+        float newY = Mathf.Sin(angle * Mathf.Deg2Rad) * (oldDirection.x) + Mathf.Cos(angle * Mathf.Deg2Rad) * (oldDirection.y);
+        float newZ = oldDirection.z;
+        return new Vector3(newX, newY, newZ);
     }
 }
